08510e95799aa59bf55dfda71ccf25d4cc46b0a7
Kazimieras B. <Judzis@gmail.com>
2025-11-14 15:12:06 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/app/dashboard/components/dashboard-client.tsx
+++ src/app/dashboard/components/dashboard-client.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import Image from "next/image";
 import {
   Table,
