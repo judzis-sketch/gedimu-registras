edc533bd4143eefcc8a610d3b7d1ee416347acb4
Kazimieras B. <Judzis@gmail.com>
2025-11-14 15:02:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8901194696-cbab9",
+  "appId": "1:983753027000:web:fb5ed890a0a76a4694734d",
+  "apiKey": "AIzaSyC1FzfverVdu0mxsMD2ilIMqt4v_6we4Ek",
+  "authDomain": "studio-8901194696-cbab9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "983753027000"
+};
